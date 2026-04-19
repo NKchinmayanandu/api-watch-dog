f3b52e346db83c5b05eaa6928c9772cf086d9d82
Nkchinmayanandu <nkchinmayanandunk@gmail.com>
2026-04-20 00:02:23 +0530

    fixing the variable name

--- app/api/routes/telegram.py
+++ app/api/routes/telegram.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN not set")
